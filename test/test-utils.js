e56f16f30a4238e0e0860b952284a5bcea9a85b1
Andrew L. Moore <slewsys@gmail.com>
2022-07-11 03:24:22 +0000

    Convert CommonJS to ESM.

--- test/test-utils.js
+++ test/test-utils.js
@@ -1,25 +1,21 @@
 'use strict';
-
-exports.MiddlewareGenerator = MiddlewareGenerator;
-
 function MiddlewareGenerator() {
-  this.count = 0;
+    this.count = 0;
 }
-
-MiddlewareGenerator.prototype.generate = function() {
-  const i = this.count += 1;
-  const self = this;
-  return async function(ctx, next) {
-    const expectedBody = (i - 1) + ' out of ' + self.count;
-    if (i > 1 && ctx.body !== expectedBody) {
-      ctx.throw(400, 'Handler executed out-of-order');
-      return;
-    }
-    ctx.body = i + ' out of ' + self.count;
-    await next();
-  };
+MiddlewareGenerator.prototype.generate = function () {
+    const i = this.count += 1;
+    const self = this;
+    return async function (ctx, next) {
+        const expectedBody = (i - 1) + ' out of ' + self.count;
+        if (i > 1 && ctx.body !== expectedBody) {
+            ctx.throw(400, 'Handler executed out-of-order');
+            return;
+        }
+        ctx.body = i + ' out of ' + self.count;
+        await next();
+    };
 };
-
-MiddlewareGenerator.prototype.getExpectedBody = function() {
-  return this.count + ' out of ' + this.count;
+MiddlewareGenerator.prototype.getExpectedBody = function () {
+    return this.count + ' out of ' + this.count;
 };
+export { MiddlewareGenerator };
